1aebcee8d10ebee117e40cb9e36b9f4c1bdf0589
Gustavo Wirzt <gwirzt@hotmail.com>
2024-07-29 18:34:40 -0300

    Dependencias Txt

--- proyecto_noticias/noticias/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import Article
-
-
-def index(request):
-    articles = Article.objects.all()
-    return render(request, 'noticias/index.html', {'articles': articles})
